9d25c56220f9c18aeb0c49ca9edf23f34d24f300
Injamul Haque <101572467+injamul3798@users.noreply.github.com>
2026-01-26 20:59:51 +0600

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,7 +4,7 @@ from .api.todo import router as todo_router
 from .database import engine
 from .models.todo import Base
 
-app = FastAPI(title="To-Do App API test01  ")
+app = FastAPI(title="To-Do App API test01 final ")
 
 # CORS middleware for frontend communication
 app.add_middleware(
